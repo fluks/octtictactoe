f807fb5e6f708a7a434d20970dd2e91b18818846
fluks <kalle.heikola@gmail.com>
2014-08-20 05:09:39 +0300

    Fix column indices
    Print odd column indices on the top and even indices to the bottom
    of the squares.

--- printSquares.m
+++ printSquares.m
@@ -1,17 +1,38 @@
 function printSquares(squares, players)
-    columnCoordinates = mat2str(1:rows(squares));
-    % Remove square brackets.
-    columnCoordinates(1) = '';
-    columnCoordinates(length(columnCoordinates)) = '';
-    printf('  %s\n', columnCoordinates);
+    s = squares;
 
-    for r = 1:rows(squares)
-        % Row coordinates.
-        printf('%i ', r);
-        for c = 1:columns(squares)
-            printf('%c ', players.chars(squares(r, c)))
-        end
-        printf('\n');
-    end
+    columnCoordinates1 = mat2str(1:2:columns(squares));
+    % Add 2 spaces after every number < 10.
+    columnCoordinates1 = regexprep(columnCoordinates1, '(\D\d)\b', '$1  ');
+    % Add 1 space after every number >= 10.
+    columnCoordinates1 = regexprep(columnCoordinates1, '(\d\d)\b', '$1 ');
+    columnCoordinates1 = regexprep(columnCoordinates1, '[\[\]]', '')
+
+    columnCoordinates2 = mat2str(2:2:columns(squares) - 1);
+    columnCoordinates2 = regexprep(columnCoordinates2, '(\D\d)\b', '$1  ');
+    columnCoordinates2 = regexprep(columnCoordinates2, '(\d\d)\b', '$1 ');
+    columnCoordinates2 = regexprep(columnCoordinates2, '[\[\]]', '')
+
+    rowCoordinates = num2str((1:rows(s))');
+    rowCoordinates(:, end + 1) = ' ';
+
+    % Transform numerical matrix to characters.
+    s = s';
+    s(s == players.x) = players.chars(players.x);
+    s(s == players.o) = players.chars(players.o);
+    s(s == players.empty) = players.chars(players.empty);
+
+    % Add padding.
+    s = kron(s, [ 1; 0]);
+    s(s == 0) = ' ';
+    s(end, :) = "\n";
+
+    s = [ rowCoordinates'; s ];
+
+    spacing = blanks(length(num2str(rows(squares))));
+    printf(' %s%s\n', spacing, columnCoordinates1);
+    printf('%c', s);
+    spacing(end) = [];
+    printf('    %s%s\n', spacing, columnCoordinates2);
     fflush(stdout);
 end
